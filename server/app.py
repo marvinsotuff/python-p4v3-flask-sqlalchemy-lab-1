37241179510fdc6ff21f8f2f47e46e0ca24dc6cc
Marvin Kioko <kiokomarvin7@gmail.com>
2026-01-14 20:25:22 +0300

    first commit

--- server/app.py
+++ server/app.py
@@ -23,5 +23,38 @@ def index():
 # Add views here
 
 
+@app.route('/earthquakes/<int:id>')
+def earthquake_show(id):
+    quake = Earthquake.query.get(id)
+    if not quake:
+        body = {"message": f"Earthquake {id} not found."}
+        return make_response(body, 404)
+
+    body = {
+        "id": quake.id,
+        "magnitude": quake.magnitude,
+        "location": quake.location,
+        "year": quake.year,
+    }
+    return make_response(body, 200)
+
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquakes_by_magnitude(magnitude):
+    quakes_q = Earthquake.query.filter(Earthquake.magnitude >= magnitude).order_by(Earthquake.id).all()
+    quakes = [
+        {
+            "id": q.id,
+            "magnitude": q.magnitude,
+            "location": q.location,
+            "year": q.year,
+        }
+        for q in quakes_q
+    ]
+
+    body = {"count": len(quakes), "quakes": quakes}
+    return make_response(body, 200)
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
